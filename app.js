3f14e4e24b4ad3d6a6b25c9dff862dc39ecb5280
hiba-hussain2004 <hibavs90@gmail.com>
2026-07-15 20:49:23 +0530

    view my post

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ app.use(Cors())
 Mongoose.connect("mongodb://hiba:hiba2004@ac-sdbjr8i-shard-00-00.qlrxeq6.mongodb.net:27017,ac-sdbjr8i-shard-00-01.qlrxeq6.mongodb.net:27017,ac-sdbjr8i-shard-00-02.qlrxeq6.mongodb.net:27017/blogdb?ssl=true&replicaSet=atlas-q5ignz-shard-0&authSource=admin&appName=Cluster0")
 
 //create a post
-app.post("/create",async(req,res)=>{
+app.post("/create", async (req, res) => {
     let input = req.body
 
     let token = req.headers.token
 
-    jwt.verify(token,"blogApp",async(error,decoded)=>{
+    jwt.verify(token, "blogApp", async (error, decoded) => {
 
-            if (decoded && decoded.email) {
-                let result = new postModel(input)
-                await result.save()
-                res.json({"status":"success"})
-                
-            } else {
-                res.json({"status":"invalid Authentication"})
-            }
+        if (decoded && decoded.email) {
+            let result = new postModel(input)
+            await result.save()
+            res.json({ "status": "success" })
+
+        } else {
+            res.json({ "status": "invalid Authentication" })
+        }
 
     })
 
@@ -37,34 +37,62 @@ app.post("/create",async(req,res)=>{
 
 //view all post
 
-app.post("/viewall",(req,res)=>{
-    
-    let token=req.headers.token
+app.post("/viewall", (req, res) => {
 
-    jwt.verify(token,"blogApp",(error,decoded)=>{
+    let token = req.headers.token
+
+    jwt.verify(token, "blogApp", (error, decoded) => {
 
         if (decoded && decoded.email) {
-                
+
             postModel.find().then(
-                (items)=>{
+                (items) => {
                     res.json(items)
                 }
             ).catch(
-                (error)=>{
-                    res.json({"status":"error"})
+                (error) => {
+                    res.json({ "status": "error" })
                 }
             )
-            
 
-            
-                
-            } else {
-                res.json({"status":"invalid Authentication"})
-            }
+
+
+
+        } else {
+            res.json({ "status": "invalid Authentication" })
+        }
 
     })
 })
+//vie my post
+app.post("/viewmypost", (req, res) => {
 
+        let input = req.body
+    let token = req.headers.token
+
+    jwt.verify(token, "blogApp", (error, decoded) => {
+
+        if (decoded && decoded.email) {
+
+            postModel.find(input).then(
+                (items) => {
+                    res.json(items)
+                }
+            ).catch(
+                (error) => {
+                    res.json({ "status": "error" })
+                }
+            )
+
+
+
+
+        } else {
+            res.json({ "status": "invalid Authentication" })
+        }
+
+    })
+})
 
 app.post("/signup", async (req, res) => {
     let input = req.body
@@ -83,7 +111,7 @@ app.post("/signup", async (req, res) => {
             } else {
 
                 let result = new usermodel(input)
-                 result.save()
+                result.save()
                 res.json({ "status": "success" })
             }
         }

b52b1d33b74f91ddc7e4e2c5e1d049691bf5b044
hiba-hussain2004 <hibavs90@gmail.com>
2026-07-15 20:23:12 +0530

    new

--- app.js
+++ app.js
@@ -4,10 +4,38 @@ const Cors = require("cors")
 const Bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const usermodel = require("./models/users")
+const postModel = require("./models/posts")
 let app = Express()
 app.use(Express.json())
 app.use(Cors())
 Mongoose.connect("mongodb://hiba:hiba2004@ac-sdbjr8i-shard-00-00.qlrxeq6.mongodb.net:27017,ac-sdbjr8i-shard-00-01.qlrxeq6.mongodb.net:27017,ac-sdbjr8i-shard-00-02.qlrxeq6.mongodb.net:27017/blogdb?ssl=true&replicaSet=atlas-q5ignz-shard-0&authSource=admin&appName=Cluster0")
+
+//create a post
+app.post("/create",async(req,res)=>{
+    let input = req.body
+
+    let token = req.headers.token
+
+    jwt.verify(token,"blogApp",async(error,decoded)=>{
+
+            if (decoded && decoded.email) {
+                let result = new postModel(input)
+                await result.save()
+                res.json({"status":"success"})
+                
+            } else {
+                res.json({"status":"invalid Authentication"})
+            }
+
+    })
+
+
+
+
+})
+
+
+
 app.post("/signup", async (req, res) => {
     let input = req.body
     let hashedPassword = Bcrypt.hashSync(req.body.password, 10)

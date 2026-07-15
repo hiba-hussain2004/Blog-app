95ffe3f8ba03f7067be2a1f18d7a49cbb20ce8d3
hiba-hussain2004 <hibavs90@gmail.com>
2026-07-15 18:49:17 +0530

    project created

--- app.js
+++ app.js
@@ -3,13 +3,41 @@ const Mongoose = require("mongoose")
 const Cors = require("cors")
 const Bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-
+const usermodel = require("./models/users")
 let app = Express()
+app.use(Express.json())
+app.use(Cors())
+Mongoose.connect("mongodb://hiba:hiba2004@ac-sdbjr8i-shard-00-00.qlrxeq6.mongodb.net:27017,ac-sdbjr8i-shard-00-01.qlrxeq6.mongodb.net:27017,ac-sdbjr8i-shard-00-02.qlrxeq6.mongodb.net:27017/blogdb?ssl=true&replicaSet=atlas-q5ignz-shard-0&authSource=admin&appName=Cluster0")
+app.post("/signup", async (req, res) => {
+    let input = req.body
+    let hashedPassword = Bcrypt.hashSync(req.body.password, 10)
+    console.log(hashedPassword)
+    req.body.password = hashedPassword
+    console.log(input)
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+
+
+    usermodel.find({ email: req.body.email }).then(
+        (items) => {
+            if (items.length > 0) {
+
+                res.json({ "status": "email ID already exist" })
+            } else {
+
+                let result = new usermodel(input)
+                 result.save()
+                res.json({ "status": "success" })
+            }
+        }
+    ).catch(
+        (error) => {
+
+        }
+    )
+
+
 })
 
-app.listen(3030,()=>{
+app.listen(3030, () => {
     console.log("Server started")
 })
